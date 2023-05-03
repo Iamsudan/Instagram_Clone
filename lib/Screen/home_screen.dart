b0f0786699f6c76b9805f13196e5b0f9181b6ce0
Iamsudan <khanalsudan89@gmail.com>
2023-05-04 00:38:33 +0545

    Simple Changes in Home Screen

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeScreem extends StatelessWidget {
-  const HomeScreem({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
