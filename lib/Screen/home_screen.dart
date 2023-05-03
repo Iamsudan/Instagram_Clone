99aa82eaffcb934b68d6830b76f1d713ba3666a1
Iamsudan <khanalsudan89@gmail.com>
2023-05-04 00:29:33 +0545

    Some changes made in Home Screen

---
+++ lib/Screen/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreem extends StatelessWidget {
+  const HomeScreem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Hi this is HomeScreen"),
+      ),
+    );
+  }
+}
