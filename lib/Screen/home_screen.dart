a86135e9894eecd2a2cc1876582997294dc5edaa
Iamsudan <khanalsudan89@gmail.com>
2023-05-05 01:40:23 +0545

    Error Occurred while adding Instagram Logo in appbar

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/Screen/login_screen.dart';
+import 'package:instagram/utils/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,14 +8,18 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Instagram'),
-        
-      ),
-      body: Container(
-
-      )
-      
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          // title: const Text('Instagram'),
+          actions: [
+            IconButton(
+                onPressed: () => const LoginScreen(),
+                icon: const ImageIcon(AssetImage('assests/ic_instagram.svg'))),
+            IconButton(
+                onPressed: () => const LoginScreen(),
+                icon: const ImageIcon(AssetImage('assests/messenger.png')))
+          ],
+        ),
+        body: Container());
   }
 }
