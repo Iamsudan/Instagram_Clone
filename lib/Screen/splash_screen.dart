99e4f745e0851b741b7daa199eae3765fd860f3f
Iamsudan <khanalsudan89@gmail.com>
2023-05-05 01:04:20 +0545

    Route after splash screen changed

--- lib/Screen/splash_screen.dart
+++ lib/Screen/splash_screen.dart
@@ -22,39 +22,40 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
     Timer(const Duration(seconds: 5), () {
-      Navigator.pushReplacement(context,
-          MaterialPageRoute(builder: (context) => StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.active) {
-              if (snapshot.hasData) {
-                return const ResponsiveLayout(
-                    webScreenLayout: WebScreenLayout(),
-                    mobileScreenLayout: MobileScreenLayout());
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text('${snapshot.error}'),
-                );
-              }
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: primaryColor,
-                ),
-              );
-            }
-            return const LoginScreen();
-          },
-        )));
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => StreamBuilder(
+                    stream: FirebaseAuth.instance.authStateChanges(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.active) {
+                        if (snapshot.hasData) {
+                          return const ResponsiveLayout(
+                              webScreenLayout: WebScreenLayout(),
+                              mobileScreenLayout: MobileScreenLayout());
+                        } else if (snapshot.hasError) {
+                          return Center(
+                            child: Text('${snapshot.error}'),
+                          );
+                        }
+                      }
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            color: primaryColor,
+                          ),
+                        );
+                      }
+                      return const LoginScreen();
+                    },
+                  )));
     });
 
-    
-      @override
-      Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-      }
+    @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      throw UnimplementedError();
+    }
   }
 
   @override
@@ -62,17 +63,12 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: mobileBackgroundColor,
       body: Center(
-        
         child: Image.asset(
           "assests/instagram.png",
-          color:const Color.fromARGB(255, 204, 27, 151),
+          color: const Color.fromARGB(255, 204, 27, 151),
           scale: 5,
         ),
-
-        
       ),
-
-      
     );
   }
 }
