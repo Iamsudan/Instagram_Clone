22739f69ca53743d269d0b76e98af50bdde8102b
Iamsudan <khanalsudan89@gmail.com>
2023-05-05 01:07:39 +0545

    Splash Screen Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -39,29 +39,7 @@ class Myapp extends StatelessWidget {
         title: 'Instagram',
         theme: ThemeData.dark()
             .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.active) {
-              if (snapshot.hasData) {
-                return const ResponsiveLayout(
-                    webScreenLayout: WebScreenLayout(),
-                    mobileScreenLayout: MobileScreenLayout());
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text('${snapshot.error}'),
-                );
-              }
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: primaryColor,
-                ),
-              );
-            }
-            return const LoginScreen();
-          },
-        ));
+            home: const SplashScreen());
+        
   }
 }
